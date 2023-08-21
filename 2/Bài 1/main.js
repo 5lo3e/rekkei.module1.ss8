cd168a11bd04df723b719d5c5c3a242f049a4123
5lo3e <dungpham197qn@gmail.com>
2023-08-21 07:57:50 +0700

    new

---
+++ 2/Bài 1/main.js
@@ -0,0 +1,21 @@
+let e,d
+a= +prompt("Nhập điểm môn Lý");
+
+b= +prompt("Nhập điểm môn Hóa");
+
+c= +prompt("Nhập điểm môn Sinh");
+
+d= a + b + c;
+e= d/3;
+document.writeln('điểm trung bình là '+e);
+document.write('<br>')
+
+document.writeln('điểm tổng là '+d);
+
+
+let q=Number(prompt("Nhập nhiệt độ cần chuyển đổi"));
+    w= ((q/5)*9 + 32);
+document.write(`độ f là ${w} `+w);
+let u=Number(prompt("Nhập bán kính hình tròn"));
+console.log(`Diện tích hình tròn là ${(q*q*3.14)}`);
+console.log(`Chu vi hình tròn là ${(q*2)*3.14}`);
---
+++ Thực hành 1/main.js
@@ -0,0 +1,5 @@
+console.log('welcometoRikkeiAcademy');
+prompt("Mời bạn nhập số tuổi của mình");
+document.writeln("Quyết tâm học lập trình javascrip");
+alert("Chào bạn đén với học viện Rekkei Academy");
+confirm("Bạn đã đủ 18 tuổi chưa");  
